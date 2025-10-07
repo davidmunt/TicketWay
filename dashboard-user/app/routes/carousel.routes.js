cd7ad169cf2f40310bff2c05c347d53da541decd
davidmunt <163463306+davidmunt@users.noreply.github.com>
2025-10-07 02:04:22 +0200

    Delete dashboard-user directory

--- dashboard-user/app/routes/carousel.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-  const carousel = require("../controllers/carousel.controller");
-
-  //GET carousel all Categories
-  app.get("/carousel", carousel.getCarouselCategory);
-
-  //GET Carousel of images from one Product
-  app.get("/carousel/:slug", carousel.getCarouselConcert);
-};
