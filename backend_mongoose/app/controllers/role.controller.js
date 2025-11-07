61f9155bc48147ba71854368a6d94bb82b5dc2a4
David <davidmunteanonti@gmail.com>
2025-11-07 20:32:33 +0100

    Mofifico controller role

--- backend_mongoose/app/controllers/role.controller.js
+++ backend_mongoose/app/controllers/role.controller.js
@@ -1,8 +1,8 @@
 const asyncHandler = require("express-async-handler");
-const argon2 = require("argon2");
 const User = require("../models/user.model");
-const UserCompany = require("../models/user.model");
-const UserAdmin = require("../models/user.model");
+const UserCompany = require("../models/userCompany.model");
+const UserAdmin = require("../models/userAdmin.model");
+const argon2 = require("argon2");
 
 const getUserRole = asyncHandler(async (req, res) => {
   try {
@@ -13,29 +13,20 @@ const getUserRole = asyncHandler(async (req, res) => {
     const loginUser = await User.findOne({ email: user.email }).exec();
     if (loginUser) {
       const match = await argon2.verify(loginUser.password, user.password);
-      if (!match) {
-        return res.status(401).json({ message: "Unauthorized" });
-      } else {
-        return res.status(200).json({ role: "user" });
-      }
+      if (!match) return res.status(401).json({ message: "Unauthorized" });
+      return res.status(200).json({ role: "user" });
     }
     const loginUserCompany = await UserCompany.findOne({ email: user.email }).exec();
     if (loginUserCompany) {
       const match = await argon2.verify(loginUserCompany.password, user.password);
-      if (!match) {
-        return res.status(401).json({ message: "Unauthorized" });
-      } else {
-        return res.status(200).json({ role: "company" });
-      }
+      if (!match) return res.status(401).json({ message: "Unauthorized" });
+      return res.status(200).json({ role: "company" });
     }
     const loginUserAdmin = await UserAdmin.findOne({ email: user.email }).exec();
     if (loginUserAdmin) {
       const match = await argon2.verify(loginUserAdmin.password, user.password);
-      if (!match) {
-        return res.status(401).json({ message: "Unauthorized" });
-      } else {
-        return res.status(200).json({ role: "admin" });
-      }
+      if (!match) return res.status(401).json({ message: "Unauthorized" });
+      return res.status(200).json({ role: "admin" });
     }
     return res.status(404).json({ message: "Usuario no encontrado" });
   } catch (error) {
