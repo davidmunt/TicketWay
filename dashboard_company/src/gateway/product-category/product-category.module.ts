62696d1664f38f4a2166400daaa0234fb341506c
David <davidmunteanonti@gmail.com>
2025-12-04 17:26:57 +0100

    Añado Middleware a endpoints de Product y ProductCategory

--- dashboard_company/src/gateway/product-category/product-category.module.ts
+++ dashboard_company/src/gateway/product-category/product-category.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ProductCategoryController } from './product-category.controller';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
+import { AuthMiddleware } from '../../middleware/auth.middleware';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { JwtCustomModule } from '../../jwt/jwt.module';
 
 @Module({
   imports: [
+    PrismaModule,
+    JwtCustomModule,
     ClientsModule.register([
       {
         name: 'PRODUCT_CATEGORY_MS',
@@ -17,4 +22,8 @@ import { ProductCategoryController } from './product-category.controller';
   ],
   controllers: [ProductCategoryController],
 })
-export class ProductCategoryModule {}
+export class ProductModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(ProductCategoryController);
+  }
+}
--- dashboard_company/src/gateway/product/product.module.ts
+++ dashboard_company/src/gateway/product/product.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ProductController } from './product.controller';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
+import { AuthMiddleware } from '../../middleware/auth.middleware';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { JwtCustomModule } from '../../jwt/jwt.module';
 
 @Module({
   imports: [
+    PrismaModule,
+    JwtCustomModule,
     ClientsModule.register([
       {
         name: 'PRODUCT_MS',
@@ -17,4 +22,8 @@ import { ProductController } from './product.controller';
   ],
   controllers: [ProductController],
 })
-export class ProductModule {}
+export class ProductModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(ProductController);
+  }
+}
