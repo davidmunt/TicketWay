6689df354cffcf257a61cecb2d34850d465b8f85
David <davidmunteanonti@gmail.com>
2025-12-20 01:53:53 +0100

    Soluciono errores dashboard_company

--- dashboard_company/src/main.ts
+++ dashboard_company/src/main.ts
@@ -5,13 +5,25 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = ['http://localhost:4200', 'http://frontend:80'];
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('API_PORT') || 3002;
   const corsOrigin = configService.get<string>('CORSURL') || '*';
 
+  // app.enableCors({
+  //   origin: corsOrigin,
+  //   credentials: true,
+  // });
+
   app.enableCors({
-    origin: corsOrigin,
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   });
 
